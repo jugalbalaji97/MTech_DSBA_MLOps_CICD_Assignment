40be4e9037030ced687e92734d9516e24dce019d
Jugal <jugalbalaji97@gmail.com>
2024-04-07 23:07:12 +0530

    testing workflows 2

--- train.py
+++ train.py
@@ -6,10 +6,12 @@ import numpy as np
 df = pd.read_csv("data/train.csv")
 X = df.drop(columns=['Disease']).to_numpy()
 y = df['Disease'].to_numpy()
-labels = np.array(['Anemia', 'Diabetes', 'Healthy', 'Heart Di', 'Thalasse', 'Thromboc'])
+labels = np.sort(np.unique(y))
+#labels = np.array(['Anemia', 'Diabetes', 'Healthy', 'Heart Di', 'Thalasse', 'Thromboc'])
 y = np.array([np.where(labels == x) for x in y]).flatten()
 
-model = GradientBoostingClassifier(max_depth = 3, n_estimators = 500, learning_rate = 0.05).fit(X,y)
+model = LogisticRegression(C=0.01, max_iter=1000).fit(X, y)
+#model = GradientBoostingClassifier(max_depth = 3, n_estimators = 500, learning_rate = 0.05).fit(X,y)
 
 with open("model.pkl", 'wb') as f:
     pickle.dump(model, f)
